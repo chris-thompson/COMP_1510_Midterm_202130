a9770b8d6a2bdd966e0d2ff1fc320ffb8049e624
Chris <chris_thompson@bcit.ca>
2021-10-18 14:26:24 -0700

    Create question 5 description

---
+++ 5.py
@@ -0,0 +1,21 @@
+"""
+The Lo Shu Magic Square is a matrix with 3 rows and 3 columns.
+
+The Lo Shu Magic Square has the following two properties:
+i.  The grid contains the numbers 1 through 9 (each number appears once)
+ii. The sum of each row, column, and each diagonal is the same number.
+
+In this example, the sum is 15:
+4   9   2
+3   5   7
+8   1   6
+
+We can simulate a magic square using a 2D list in Python.
+Define a function called validate_lo_shu that accepts a 2D list as an argument.
+The function must return True if the list is a Lo Shu Magic Square.
+The function must return Return False if it is anything else.
+
+You may not modify the 2D list passed as an argument in any way, even temporarily.
+You must create two useful doctests for this function.
+No main function is required.
+"""
