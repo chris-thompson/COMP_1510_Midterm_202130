d9d8a1b616a8f4c65b7262f825b3ca5699ffa062
Chris <chris_thompson@bcit.ca>
2021-10-18 14:39:45 -0700

    Create question 2 description

---
+++ 2.py
@@ -0,0 +1,16 @@
+"""
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get:
+
+3, 5, 6 and 9
+
+The sum of these multiples is 23.
+
+Define a function called pointless_flex that accepts a single parameter, a positive
+non-zero integer called upper_bound. The function calculates and returns the sum of all
+the multiples of 3 or 5 strictly below upper_bound, i.e., in the range (0, upper_bound).
+
+You must provide a full docstring for this function including all pre- and post-conditions.
+
+You must create two useful doctests for this function.
+No main function is required.
+"""
